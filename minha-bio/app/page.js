8f278a1742bbf222962c6cf52729254b6b187c88
Bento Guilherme <bento.00000852680@unicap.br>
2026-03-06 20:26:33 +0000

    celular ok pt2

--- minha-bio/app/page.js
+++ minha-bio/app/page.js
@@ -1,12 +1,17 @@
 import Image from "next/image";
+import Head from "next/head";
+
+<Head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</Head>
 
 export default function Home() {
   return (
     <body>
-      <div class="quadrado">
+      <div className="quadrado">
         <img src="foto.jpeg" alt="Foto de Bento"></img>
       
-        <div class="texto">  
+        <div className="texto">  
           <h1>Bento Guilherme Gomes Oliveira</h1>
 
           <p>Oi! Meu nome é Bento, tenho 21 anos e atualmente curso Ciência da Computação na UNICAP. Gosto de jogar diversos tipos de jogos, especialmente com minha namorada Lorenna, amo ver filmes, séries e amo meus gatinhos, Timbó e Tata.</p>
