5065cc98d0884b811ea68e49e1ab990ff3dfd225
Bento Guilherme <bento.00000852680@unicap.br>
2026-03-06 20:01:52 +0000

    bio completa

--- minha-bio/app/page.js
+++ minha-bio/app/page.js
@@ -2,6 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <title>Minha bio!</title>
+    <body>
+      <div class="quadrado">
+        <img src="foto.jpeg" alt="Foto de Bento"></img>
+      
+        <div class="texto">  
+          <h1>Bento Guilherme Gomes Oliveira</h1>
+
+          <p>Oi! Meu nome é Bento, tenho 21 anos e atualmente curso Ciência da Computação na UNICAP. Gosto de jogar diversos tipos de jogos, especialmente com minha namorada Lorenna, amo ver filmes, séries e amo meus gatinhos, Timbó e Tata.</p>
+        </div>
+      </div>
+    </body>
   );
 }
