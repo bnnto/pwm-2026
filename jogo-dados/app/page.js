ae5a541596529046014a525c938347a5aae2d8b6
Bento Guilherme <gbento555@gmail.com>
2026-04-01 13:40:15 -0300

    pensando..

--- jogo-dados/app/page.js
+++ jogo-dados/app/page.js
@@ -8,15 +8,30 @@ export default function Home() {
   const [jogador1, setJogador1] = useState({ nome: "Jogador 1", pontos: 0 });
   const [jogador2, setJogador2] = useState({ nome: "Jogador 2", pontos: 0 });
 
+  const [rodada, setRodada] = useState({ rodada: 1, jogadorAtual: 0, pontos: [0, 0]});
+
   function rolarDado(jogador, setJogador){
     const valor = Math.floor(Math.random() * 6) + 1;
 
     setJogador({ ...jogador, dado: valor, pontos: jogador.pontos + valor });
   }
   
+  setRodada(estadoAtual => ({
+    ...estadoAtual,
+    rodada: estadoAtual.rodada + 1,
+    jogadorAtual: 1 - estadoAtual.jogadorAtual,
+    pontos: jogadorAtual.pontos,
+  }));
+
+  const [p1, p2] = rodada.pontos;
+  if(p1 > p2) return `${nome[0]} venceu!`;
+  if(p1 < p2) return `${nome[1]} venceu!`;
+  return "Empate!";
+
   return (
     <>
     <div>
+      <h3>Rodada: {rodada.valor}</h3>
       <div>
       <JogoDados
         jogador={jogador1}
